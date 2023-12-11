9388e0aa0dbd85d9c919d4666bcc14d7b48b489f
1001-Kiernan-Dillon <97993388+1001-Kiernan-Dillon@users.noreply.github.com>
2023-12-10 21:20:17 -0800

    correct kml for geoip2

--- driver.py
+++ driver.py
@@ -1,91 +1,84 @@
-# Import necessary libraries
 import dpkt
 import socket
 import geoip2.database
-import escape
 
 # Initialize the GeoIP database
 reader = geoip2.database.Reader('GeoLite2-City.mmdb')
-# Define the main function
+
+
 def main():
-
     # Open a PCAP file for reading
-    filename='wire.pcap'
-    f = open(filename, 'rb')
-    pcap = dpkt.pcap.Reader(f)
-    plotIPs(pcap)
-    # Define KML header with a style for the KML file
-    kmlheader = '<?xml version="1.0" encoding="UTF-8"?> \n<kml xmlns="http://www.opengis.net/kml/2.2">\n<Document>\n' \
-                '<Style id="transBluePoly">' \
-                '<LineStyle>' \
-                '<width>1.5</width>' \
-                '<color>501400E6</color>' \
-                '</LineStyle>' \
-                '</Style>'
+    filename = 'wire.pcap'
+    with open(filename, 'rb') as f:
+        pcap = dpkt.pcap.Reader(f)
+        kml_content, visited_cities = plot_ips(pcap)
+        save_to_kml(kml_content)
+        print_unique_cities(visited_cities)
 
-    # Define KML footer
-    kmlfooter = '</Document>\n</kml>\n'
-    # Create KML document by combining header, IP plotting, and footer
-    kmldoc = kmlheader + plotIPs(pcap) + kmlfooter
 
-    # Print the KML document
-    print(kmldoc)
+def plot_ips(pcap):
+    kml_pts = ''
+    visited_cities = set()
 
-# Define a function to plot IP addresses in KML format
-def plotIPs(pcap):
-    kmlPts = ''
     for (ts, buf) in pcap:
         try:
             eth = dpkt.ethernet.Ethernet(buf)
             ip = eth.data
             src = socket.inet_ntoa(ip.src)
             dst = socket.inet_ntoa(ip.dst)
-            KML = retKML(dst, src)
-            kmlPts = kmlPts + KML
-        except:
+            kml, dst_city = ret_kml(dst, src, visited_cities)
+            if kml:
+                kml_pts += kml
+        except Exception as e:
+            # Handle specific exceptions if needed
+            print(f"Exception in plot_ips: {e}")
             pass
-    return kmlPts
+    return kml_pts, visited_cities
 
-# Define a function to generate KML for a given source and destination IP
-def retKML(dstip, srcip):
+
+def ret_kml(dst_ip, src_ip, visited_cities):
     try:
-        dst_response = reader.city(dstip)
+        dst_response = reader.city(dst_ip)
         src_response = reader.city('143.170.82.70')  # Replace with an actual IP address
 
-        dst_city = dst_response.city.name
-        if(dst_city == 'None'):
-            dst_city = dst_response.country.name
+        dst_city = dst_response.city.name or dst_response.country.name
         src_city = src_response.city.name
 
-       #dst_longitude = dst_response.location.longitude
-        #dst_latitude = dst_response.location.latitude
-        #src_longitude = src_response.location.longitude
-        #src_latitude = src_response.location.latitude
-
-        print(f"Destination IP: {dstip}, City: {dst_city}")
+        print(f"Destination IP: {dst_ip}, City: {dst_city}")
         print(f"Source IP: 143.170.82.70, City: {src_city}")
 
-        #kml = (
-        #    '<Placemark>\n'
-        #    '<name>%s</name>\n'
-        #    '<extrude>1</extrude>\n'
-        #    '<tessellate>1</tessellate>\n'
-        #    '<styleUrl>#transBluePoly</styleUrl>\n'
-       #     '<LineString>\n'
-       #     '<coordinates>%f,%f\n%f,%f</coordinates>\n'
-        #    '</LineString>\n'
-        #    '</Placemark>\n'
-       # ) % (escape(dstip), dst_longitude, dst_latitude, src_longitude, src_latitude)
-
-        #return kml
+        # Return KML content for the placemark if it's a new city
+        if dst_city and dst_city not in visited_cities:
+            visited_cities.add(dst_city)
+            return f"<Placemark><name>{dst_city}</name><Point><coordinates>{dst_response.location.longitude},{dst_response.location.latitude},0</coordinates></Point></Placemark>", dst_city
+        else:
+            return '', ''
 
     except geoip2.errors.AddressNotFoundError:
         # Handle the case where the GeoIP data is not available
-        return ''
+        return '', ''
     except Exception as e:
         # Handle other exceptions
-        print(f"Exception in retKML: {e}")
-        return ''
+        print(f"Exception in ret_kml: {e}")
+        return '', ''
+
+
+def save_to_kml(kml_content):
+    kml_header = '<?xml version="1.0" encoding="UTF-8"?>\n' \
+                  '<kml xmlns="http://www.opengis.net/kml/2.2">\n' \
+                  '<Document>\n'
+    kml_footer = '</Document>\n</kml>'
+    with open('visited_cities.kml', 'w') as kml_file:
+        kml_file.write(kml_header + kml_content + kml_footer)
+    print("KML file saved as 'visited_cities.kml'")
+
+
+def print_unique_cities(visited_cities):
+    print("\nUnique Cities Visited:")
+    for city in sorted(visited_cities):
+        print(city)
+    print(f"\nTotal number of unique cities visited: {len(visited_cities)}")
+
 
 if __name__ == "__main__":
     main()
